099a782b3ec95dc47bbdc1a0d2838dc1637a2b9a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-16 18:41:08 +0200

    cd..

---
+++ src/main/kotlin/com/example/KotlinRepository/KotlinRepositoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.KotlinRepository
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRepositoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRepositoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/KotlinRepository/KotlinRepositoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KotlinRepository
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRepositoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
