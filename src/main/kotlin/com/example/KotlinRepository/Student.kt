54ed2d1e38b545cd5fdff51504fbd4c6be29245f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-30 18:36:51 +0100

    cd..

--- src/main/kotlin/com/example/KotlinRepository/Student.kt
+++ src/main/kotlin/com/example/KotlinRepository/Student.kt
@@ -13,4 +13,14 @@ data class Student(
 {
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         val id:Long = 0
+
+        fun createStudents(n:Int,name:String,surname:String,f:(Int)->String):ArrayList<Student>{
+                val array:ArrayList<Student> = arrayListOf()
+
+                for(i in 1..n){
+                        array.add(Student("${name}_${f(n)}","${surname}_${f(n)}"))
+                }
+
+                return array
+        }
 }
